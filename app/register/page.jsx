9b89eac2aea2fca702b2673da95f58ec5097132c
Ranindu Geekiyanage <ranindu.rag@gmail.com>
2024-12-31 16:44:51 +0530

    register page added

---
+++ app/register/page.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import RegisterPage from "./register-page";
+
+const Register = () => {
+  return (
+    <div className="flex flex-col justify-center items-center min-h-screen text-black">
+      <RegisterPage />;
+    </div>
+  );
+};
+
+export default Register;
--- app/register/register-page.jsx
+++ app/register/register-page.jsx
@@ -1,27 +1,13 @@
-"use client";
-import Link from "next/link";
-//Client component for CSR
+import React from "react";
 
-import { useState } from "react";
-
-export default function LoginForm() {
-  // setter functions for input fields
-  const [email, setEmail] = useState("");
-  const [password, setPassword] = useState("");
-
-  //   submit function
-  const handleSubmit = (e) => {
-    e.preventDefault();
-    console.log("Form Data", { email: email, passowrd: password });
-  };
-
+const RegisterPage = () => {
   return (
     <div className="w-[380px] mx-auto">
       <div className="bg-white shadow-lg border border-gray-200 rounded-lg p-4">
-        <form className="space-y-6" onSubmit={handleSubmit}>
+        <form className="space-y-6">
           {/* Title */}
           <h3 className="text-center text-xl font-semibold text-gray-900">
-            Sign in to Evotech
+            Register an Account
           </h3>
 
           {/* Email */}
@@ -36,8 +22,6 @@ export default function LoginForm() {
               type="email"
               name="email"
               id="email"
-              value={email}
-              onChange={(e) => setEmail(e.target.value)}
               placeholder="yourname@email.com"
               className="bg-gray-50 border border-gray-300 rounded-lg text-gray-900  focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5"
             />
@@ -56,8 +40,6 @@ export default function LoginForm() {
               name="password"
               id="password"
               placeholder="password"
-              value={password}
-              onChange={(e) => setPassword(e.target.value)}
               className="bg-gray-50 border border-gray-300 rounded-lg text-gray-900 focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 "
             />
           </div>
@@ -99,12 +81,14 @@ export default function LoginForm() {
 
           <div className="flex justify-center text-sm font-medium text-gray-500 space-x-1">
             <span>Not registerd?</span>
-            <Link href="/register" className="text-blue-700 hover:underline">
+            <a href="/register" className="text-blue-700 hover:underline">
               Create an account
-            </Link>
+            </a>
           </div>
         </form>
       </div>
     </div>
   );
-}
+};
+
+export default RegisterPage;
